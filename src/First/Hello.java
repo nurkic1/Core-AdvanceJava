42add74f09fb16e5012b031e2ab265ce2d3a81be
mnurkiczzz <nurkicmehmed05@gmail.com>
2023-01-30 21:42:35 +0100

    Heeej

---
+++ src/First/Hello.java
@@ -0,0 +1,7 @@
+package First;
+
+public class Hello {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Holaa.java
+++
@@ -1,5 +0,0 @@
-public class Holaa {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
